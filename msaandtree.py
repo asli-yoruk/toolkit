4e82508fdd3310e6a4c9effb5e34bff3ae522484
asli-yoruk <asli-yoruk@hotmail.com>
2020-06-29 09:41:24 +1000

    added new files and a new version of MAKETABLE

---
+++ msaandtree.py
@@ -0,0 +1,15 @@
+import os
+
+redundancy = [99,98,95,90,80,70]
+def main():
+        for rr in redundancy:
+                for evalue in [1e-100, 1e-75, 1e-50, 1e-20, 1e-10, 1e-5]:
+                        filename =  str(rr) + '-'+ str(evalue)
+                        os.system('clustalo -i dataset-' + filename + 'copuwSubfam.fa -o dataset-' + filename + '.aln --output-order=tree-order --threads=5 --force')
+        for rr in redundancy:
+                for evalue in [1e-100, 1e-75, 1e-50, 1e-20, 1e-10, 1e-5]:
+                        filename =  str(rr) + '-'+ str(evalue)
+                        os.system("FastTree -out dataset-" + filename + ".nwk dataset-" + filename + ".aln")
+
+if __name__ == "__main__":
+    main()
