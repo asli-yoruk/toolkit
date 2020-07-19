c86417ddfd2f2ab9ac7b29a88063d08a2a36056e
asli-yoruk <asli-yoruk@hotmail.com>
2020-07-20 09:00:04 +1000

    msaandtree.py changed

--- msaandtree.py
+++ msaandtree.py
@@ -5,7 +5,7 @@ def main():
         for rr in redundancy:
                 for evalue in [1e-100, 1e-75, 1e-50, 1e-20, 1e-10, 1e-5]:
                         filename =  str(rr) + '-'+ str(evalue)
-                        os.system('clustalo -i dataset-' + filename + 'copuwSubfam.fa -o dataset-' + filename + '.aln --output-order=tree-order --threads=5 --force')
+                        os.system('clustalo -i dataset-' + filename + '_reduced.fa -o dataset-' + filename + '.aln --output-order=tree-order --threads=5 --force')
         for rr in redundancy:
                 for evalue in [1e-100, 1e-75, 1e-50, 1e-20, 1e-10, 1e-5]:
                         filename =  str(rr) + '-'+ str(evalue)
